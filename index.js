a8c82a1ac41d7d0885528df9dcc22ae38a5f00e5
QuarkNerd <garora1410@yahoo.co.uk>
2020-06-08 17:03:57 +0100

    this scene just got litt up

--- index.js
+++ index.js
@@ -11,15 +11,22 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 10;
+
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.y = 0
+directionalLight.position.z = 10
+scene.add(directionalLight);
+
+renderer.setClearColor(0xCDCDCD, 1);
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.07;
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
