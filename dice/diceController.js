05a8623f5f70edc9688c3d411fcd7667bed6030e
QuarkNerd <garora1410@yahoo.co.uk>
2020-06-20 11:23:59 +0100

    create spirals

--- dice/diceController.js
+++ dice/diceController.js
@@ -7,6 +7,26 @@ const diceByString = {
     "D20" : D20,
 }
 
+function calculateCoorForSpiral(n) {
+    const sqrt_n = Math.floor(Math.sqrt(n));
+    const anchor_square_root = sqrt_n % 2 === 1 ? sqrt_n : sqrt_n - 1; // square root of the largest odd square less than n
+    const anc_coor = (anchor_square_root - 1) / 2 // value of (anc_coor, -anc_coor) = anchor_square_root 
+
+    const diff = n - anchor_square_root ** 2;
+
+    if (diff === 0) return { x: anc_coor, y: -anc_coor };
+    if (0 < diff && diff <= 2 * anc_coor + 2) {
+        return { x: anc_coor + 1, y: diff - anc_coor - 1 };
+    }
+    if (2 * anc_coor + 2 < diff && diff <= 4 * anc_coor + 4) {
+        return { x: - diff + 3 * anc_coor + 3, y: anc_coor + 1 };
+    }
+    if (4 * anc_coor + 4 < diff && diff <= 6 * anc_coor + 6) {
+        return { x: -anc_coor - 1, y: + 5 * anc_coor + 5 - diff };
+    }
+    return { x: -7 * anc_coor - 7 + diff , y: -anc_coor -1 };
+}
+
 class DiceController {
     constructor(scene, width, height) {
         this.scene = scene;
@@ -17,9 +37,11 @@ class DiceController {
     addDie(type, colorHex = 0x00FF00) {
         const dieClass = diceByString[type];
         const newDie = new dieClass(this.scene, colorHex);
-        //move die
+        const n = this.diceList.push(newDie);
+        const { x, y } = calculateCoorForSpiral(n)
+        newDie.mesh.position.x = x; 
+        newDie.mesh.position.y = y; 
         newDie.startRoll();
-        this.diceList.push(newDie);
     }
 
     tick() {
