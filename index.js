d59e0be3d8cfb639a167f0d0f1c4e3b3aa99827c
QuarkNerd <garora1410@yahoo.co.uk>
2020-06-20 11:23:24 +0100

    create buttons to add die

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ import DiceController from './dice/diceController.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  window.innerWidth / (window.innerHeight - 200),
   0.1,
   1000
 );
-camera.position.z = 10;
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight - 200);
+document.getElementById("diceBoard").appendChild(renderer.domElement);
 
 const diceController = new DiceController(scene, window.innerWidth, window.innerHeight - 200);
-diceController.addDie("D4");
 
 var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.y = 4
-directionalLight.position.z = 10
+directionalLight.position.z = 30
 scene.add(directionalLight);
 renderer.setClearColor(0xCDCDCD, 1);
 
@@ -30,3 +28,10 @@ function animate() {
   requestAnimationFrame(animate);
 }
 animate();
+
+document.getElementById("insert_D4").addEventListener('click', () => diceController.addDie("D4"));
+document.getElementById("insert_D6").addEventListener('click', () => diceController.addDie("D6"));
+document.getElementById("insert_D8").addEventListener('click', () => diceController.addDie("D8"));
+document.getElementById("insert_D10").addEventListener('click', () => diceController.addDie("D10"));
+document.getElementById("insert_D12").addEventListener('click', () => diceController.addDie("D12"));
+document.getElementById("insert_D20").addEventListener('click', () => diceController.addDie("D20"));
